9becb60c8dd94d4c9086f11bdf00246e29202c6d
Ryan Santos <ryan.ribeiro@maeztra.com>
2022-03-29 17:22:29 -0300

    first commit

---
+++ react/Greeting.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const HelloWorld: StorefrontFunctionComponent = () => {
+  return (
+    <>
+      <h1> Hello World</h1>
+    </>
+  )
+}
+
+export default HelloWorld;
+
+// import React from 'react'
+
+// type Props = {
+//   name: string
+// }
+
+// function Greeting({ name }: Props) {
+//   return <div>Hey, {name}</div>
+// }
+
+// export default Greeting
